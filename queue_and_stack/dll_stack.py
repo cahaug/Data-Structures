be5628d6eb918b48939ad3b7fc0f1316476c62c9
cahaug <caleahaug@gmail.com>
2020-03-11 18:14:05 -0700

    stack working

--- queue_and_stack/dll_stack.py
+++ queue_and_stack/dll_stack.py
@@ -2,17 +2,61 @@ import sys
 sys.path.append('../doubly_linked_list')
 from doubly_linked_list import DoublyLinkedList
 
+
+# queue == first in first out
+# stack == first in last out
+
 class Stack:
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+        # it is in essence a list we can use for our stack
+        self.storage = DoublyLinkedList()
+
+    def __str__(self):
+        if self.storage.head is None and self.storage.tail is None:
+            return "empty"
+        curr_node = self.storage.head
+        output = ""
+        output += f'({curr_node.value})'
+        while curr_node.next is not None:
+            curr_node = curr_node.next
+            output += f' <-> ({curr_node.value})'
+        return output
 
     def push(self, value):
-        pass
+        #add item to head of stack, increase size by 1
+        # self.storage.add_to_tail(value)
+        self.storage.add_to_tail(value)
+        self.size += 1
 
     def pop(self):
-        pass
+        # if the stack contains an item, remove it from from the rear
+        if self.size > 0:
+            self.size -= 1
+            # self.storage.remove_from_tail()
+            return self.storage.remove_from_tail()
 
     def len(self):
-        pass
+        return self.size
+
+myStack = Stack()
+
+print("Adding 6")
+myStack.push(6)
+print(myStack)
+print("Adding 7")
+myStack.push(7)
+print(myStack)
+print("Adding 2")
+myStack.push(2)
+print(myStack)
+print("Adding 4")
+myStack.push(4)
+print(myStack)
+
+remVal = myStack.pop()
+print(f'Popped from tail: {remVal}')
+print(myStack)
+
+print(f'Stack size now is at {myStack.len()}')
